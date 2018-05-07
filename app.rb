da64d3c21e250ac9063424339e05c8cb8d64e2a8
Stephen Harrington <stephenharrington04@yahoo.com>
2018-05-07 19:36:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,4 +18,11 @@ class App < Sinatra::Base
     @user_name = params[:name]
     "Goodbye, #{@user_name}."
   end
+
+  get "/multiple/:num1/:num2" do
+    @num1 = params[:num1]
+    @num2 = params[:num2]
+    
+  end
+    
 end
